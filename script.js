12423412471d0ce7a7427e99bf1cd2778f327247
Jef-king <147884623+Jef-king@users.noreply.github.com>
2024-03-30 00:57:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function validEmail(str) {
-  //your JS code here.
+	const regex = /^(([^<>()[\\]\\\\.,;:\\s@\"]+(\\.[^<>()[\\]\\\\.,;:\\s@\"]+)*)|.(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email);
 }
 
 // Do not change the code below.
